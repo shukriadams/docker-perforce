44a2f9dddefa10f467670044da26f05b04153811
Shukri Adams <shukri.adams@gmail.com>
2024-09-04 14:12:04 +0200

    script refactor, better trust

--- perforce-server/build.sh
+++ perforce-server/build.sh
@@ -9,5 +9,6 @@ docker rmi shukriadams/perforce-server:latest-2020 -f
 
 docker build -t shukriadams/perforce-server .
 docker tag shukriadams/perforce-server:latest shukriadams/perforce-server:latest-2020
-docker-compose up
+docker-compose up -d
+sleep 5
 docker logs perforce-test
