6a66f91c805363358b06dd879c92ed67099b39e9
Shukri Adams <shukri.adams@gmail.com>
2025-01-03 14:14:42 +0100

    fix build path invoke

--- src/build.sh
+++ src/build.sh
@@ -3,20 +3,19 @@ set -e
 
 DOCKERPUSH=0
 SMOKETEST=0
-BUILD=1
+BUILD=0
 BRANCH=2020
 
 while [ -n "$1" ]; do 
     case "$1" in
-    --dockerpush) DOCKERPUSH=1 ;;
+    --push) DOCKERPUSH=1 ;;
     --test) SMOKETEST=1 ;;
-    --nobuild) BUILD=0 ;;
+    --build) BUILD=1 ;;
     esac 
     shift
 done
 
 if [ $BUILD -eq 1 ]; then
-    cd perforce-server
     docker-compose down
     sudo rm -rf  ./../tmp
 
@@ -27,16 +26,19 @@ if [ $BUILD -eq 1 ]; then
 
     docker build -t shukriadams/perforce-server .
     docker tag shukriadams/perforce-server:latest shukriadams/perforce-server:latest-$BRANCH
-    cd -
+    echo "build complete"
+else
+    echo "build skipped, use --build to enable"
 fi
 
 if [ $SMOKETEST -eq 1 ]; then
-    cd perforce-server
     docker-compose down
     docker-compose up -d
     sleep 5
     docker logs perforce-test
-    cd -
+    echo "test complete, note that this script doesn't yet read for explicit pass flag from container logs"
+else
+    echo "test skipped, use --test to enable"
 fi
 
 if [ $DOCKERPUSH -eq 1 ]; then
@@ -44,6 +46,9 @@ if [ $DOCKERPUSH -eq 1 ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASS 
     docker tag shukriadams/perforce-server:latest-$BRANCH shukriadams/perforce-server:$BRANCH-$TAG 
     docker push shukriadams/perforce-server:$BRANCH-$TAG
-
     echo "Push complete"
+else
+    echo "push to docker skipped, use --push to enable"
 fi
+
+echo "Done!"
