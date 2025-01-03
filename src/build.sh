174d40f1c6fa720e657d79025fbb63524070c4b9
Shukri Adams <shukri.adams@gmail.com>
2025-01-03 14:24:27 +0100

    fixed compose

--- src/build.sh
+++ src/build.sh
@@ -5,18 +5,20 @@ DOCKERPUSH=0
 SMOKETEST=0
 BUILD=0
 BRANCH=2020
+COMPOSE="docker-compose"
 
 while [ -n "$1" ]; do 
     case "$1" in
-    --push) DOCKERPUSH=1 ;;
-    --test) SMOKETEST=1 ;;
-    --build) BUILD=1 ;;
+    --push|-p) DOCKERPUSH=1 ;;
+    --test|-t) SMOKETEST=1 ;;
+    --build|-b) BUILD=1 ;;
+    --github-compose) COMPOSE="docker compose" ;;
     esac 
     shift
 done
 
 if [ $BUILD -eq 1 ]; then
-    docker-compose down
+    $COMPOSE down
     sudo rm -rf  ./../tmp
 
     # force remove existing image to all layers rebuild, this is for local environments only. on github 
@@ -32,8 +34,8 @@ else
 fi
 
 if [ $SMOKETEST -eq 1 ]; then
-    docker-compose down
-    docker-compose up -d
+    $COMPOSE down
+    $COMPOSE up -d
     sleep 5
     docker logs perforce-test
     echo "test complete, note that this script doesn't yet read for explicit pass flag from container logs"
